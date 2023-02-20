4d7d7b29366f26516285eabeb34a8351d16aae71
PedroDuarte9 <p.fmarket1996@gmail.com>
2023-02-20 17:44:01 -0300

    Test-Driven-Development

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -20,5 +20,23 @@ class ReajusteServiceTest {
 		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
+	
+	@Test
+	void ReajusteSalarioBom() {
+		ReajusteService service = new ReajusteService();
+		Funcionario funcionario  = new Funcionario("Pedro", LocalDate.now(), new BigDecimal("1000.00")); 
+		service.concederReajuste(funcionario, Desempenho.BOM);
+		
+		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
+	}
+	
+	@Test
+	void ReajusteSalarioOtimo() {
+		ReajusteService service = new ReajusteService();
+		Funcionario funcionario  = new Funcionario("Pedro", LocalDate.now(), new BigDecimal("1000.00")); 
+		service.concederReajuste(funcionario, Desempenho.OTIMO);
+		
+		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
+	}
 		
 }
